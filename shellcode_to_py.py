13db97b5ffb663e5a046ff975255e4b593107a81
Your Name <you@example.com>
2019-03-12 13:31:53 -0400

    Added shellcode_to_py script

---
+++ shellcode_to_py.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import argparse
+import subprocess
+import re
+
+MAX_CHARS_PER_LINE = 80
+
+READELF_ARGV = ["readelf", "-x", ".text"]
+
+def main():
+	parse_args()
+	completed = subprocess.run(READELF_ARGV, stdout=subprocess.PIPE)
+	readelf_output = completed.stdout.decode("utf-8")
+	readelf_output = readelf_output.split('\n')
+
+	output_string = ""
+	for x in readelf_output[2:]:
+		output_string += ''.join(x.split(" ")[3:7])
+
+	output_string = bytes.fromhex(output_string)
+	for i in range(0, len(output_string), 16):
+		print('shellcode += \'\\x' + '\\x'.join(hex(char)[2:].zfill(2) for char in output_string[i:i+16]) + '\'')
+
+def parse_args():
+	parser = argparse.ArgumentParser(description="Dumps shellcode from binary's main functon")
+	parser.add_argument('--binary', dest='bin', required=True, help='Target ELF Binary')
+	args = parser.parse_args()
+	global READELF_ARGV
+	READELF_ARGV.append(args.bin)
+
+if __name__ == "__main__":
+	main()
